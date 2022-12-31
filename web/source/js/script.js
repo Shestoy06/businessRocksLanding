8d9fcca935dcac126ecbad782caea975e7468af6
Shestoy06 <pochta5550@gmail.com>
2022-12-31 16:20:34 +0100

    add body lock while menu burger

--- web/source/js/script.js
+++ web/source/js/script.js
@@ -5,6 +5,7 @@ const menuBody = document.querySelector('.menu__body')
 menuIcon.addEventListener('click', function(e) {
     menuIcon.classList.toggle('_active')
     menuBody.classList.toggle('_active')
+    document.body.classList.toggle('_lock')
 
 })
 
